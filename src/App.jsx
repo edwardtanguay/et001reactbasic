e16aa38bae07349f1f5755af3155b0ec29aef846
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 15:33:07 +0200

    german nouns showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import './App.scss';
+import germanNouns from './data/germanNouns.json';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Test Site</h1>
-      <p>This is a test.</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Test Site</h1>
+			<h2>German Nouns</h2>
+			<p>There are {germanNouns.length} German nouns.</p>
+			{germanNouns.map((germanNoun) => {
+				return (
+					<div className="germanNoun">
+						<div>{germanNoun.singular}</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
-export default App
+export default App;
