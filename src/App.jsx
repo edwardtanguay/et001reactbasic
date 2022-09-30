9189b700c57e6a11674a2ebf9f906590e72963fa
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 15:40:50 +0200

    randomized array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import './App.scss';
-import germanNouns from './data/germanNouns.json';
+import _germanNouns from './data/germanNouns.json';
+
+function shuffle(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex]
+		];
+	}
+
+	return array;
+}
+
+// const germanNouns = shuffle(_germanNouns).filter(m => m.article === 'das');
+const germanNouns = shuffle(_germanNouns);
 
 function App() {
 	return (
@@ -11,7 +34,9 @@ function App() {
 				{germanNouns.map((germanNoun) => {
 					return (
 						<div className="germanNoun">
-							<div className="singular">{germanNoun.article} {germanNoun.singular}</div>
+							<div className="singular">
+								{germanNoun.article} {germanNoun.singular}
+							</div>
 							<div className="plural">{germanNoun.plural}</div>
 						</div>
 					);
