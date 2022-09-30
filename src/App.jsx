edfde7d243a811f35ec5e69bdc399e99c40eed7a
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 16:09:17 +0200

    german nouns showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
 import './App.scss';
-import _germanNouns from './data/germanNouns.json';
-
-function shuffle(array) {
-	let currentIndex = array.length,
-		randomIndex;
-	while (currentIndex != 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
-		[array[currentIndex], array[randomIndex]] = [
-			array[randomIndex],
-			array[currentIndex]
-		];
-	}
-
-	return array;
-}
-
-// const germanNouns = shuffle(_germanNouns).filter(m => m.article === 'das');
-const germanNouns = shuffle(_germanNouns);
+import {GermanNouns} from './components/GermanNouns';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Test Site</h1>
-			<h2>German Nouns</h2>
-			<p>There are {germanNouns.length} German nouns.</p>
-			<div className="germanNouns">
-				{germanNouns.map((germanNoun) => {
-					return (
-						<div className="germanNoun">
-							<img src="images/cardback.jpg" />
-							<div className="info">
-								<div className="singular">
-									{germanNoun.article} {germanNoun.singular}
-								</div>
-								<div className="plural">
-									{germanNoun.plural}
-								</div>
-							</div>
-						</div>
-					);
-				})}
-			</div>
+			<GermanNouns/>
 		</div>
 	);
 }
---
+++ src/components/GermanNouns.jsx
@@ -0,0 +1,44 @@
+import _germanNouns from '../data/germanNouns.json';
+
+function shuffle(array) {
+	let currentIndex = array.length,
+		randomIndex;
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex]
+		];
+	}
+
+	return array;
+}
+
+// const germanNouns = shuffle(_germanNouns).filter(m => m.article === 'das');
+const germanNouns = shuffle(_germanNouns);
+export const GermanNouns = () => {
+	return (
+		<>
+			<h2>German Nouns</h2>
+			<p>There are {germanNouns.length} German nouns.</p>
+			<div className="germanNouns">
+				{germanNouns.map((germanNoun) => {
+					return (
+						<div className="germanNoun">
+							<img src="images/cardback.jpg" />
+							<div className="info">
+								<div className="singular">
+									{germanNoun.article} {germanNoun.singular}
+								</div>
+								<div className="plural">
+									{germanNoun.plural}
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
